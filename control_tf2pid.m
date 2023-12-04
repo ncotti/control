769b84267e5641d1d13042971d02493e3c24e83f
ncotti <ncotti@frba.utn.edu.ar>
2023-12-04 19:47:50 -0300

    Digital PID

--- control_tf2pid.m
+++ control_tf2pid.m
@@ -10,39 +10,62 @@
 %
 % @Author:
 %   Nicolas Gabriel Cotti (ngcotti@gmail.com)
-function [Kp, Ki, Kd] = control_tf2pid(sys)
+function [Kp, Ki, Kd] = control_tf2pid(sys, T)
     arguments
         sys tf
+        T double = -1
     end
     
     sys
-    [num, den] = tfdata(sys, "v")
+    [num, den] = tfdata(sys, "v");
 
-    % Transfer for PID:
-    % PID(s) = ( s^2*Kd + s*Kp + KI ) / s
-    if ((length(num) == 3) && (den(2) == 1) && (den(end) == 0))
-        disp("PID (Proportional-integral-derivative)")
-        Kp = num(2)
-        Kd = num(1)
-        Ki = num(3)
+    % Analogic PID
+    if (T == -1)
+        % Transfer for PID:
+        % PID(s) = ( s^2*Kd + s*Kp + KI ) / s
+        if ((length(num) == 3) && (den(2) == 1) && (den(end) == 0))
+            disp("PID (Proportional-integral-derivative)")
+            Kp = num(2)
+            Kd = num(1)
+            Ki = num(3)
+    
+        % Transfer for PI:
+        % PI(s) = ( s*Kp + KI ) / s
+        elseif ((length(num) == 2) && (den(1) == 1) && (den(end) == 0))
+            disp("PI (proportional-integral)")
+            Kp = num(1)
+            Ki = num(2)
+            kd = 0
+        % Transfer for PD:
+        % PD(s) = s*Kd + Kp
+        elseif ((length(num) == 2) && isempty(den))
+            disp("PD (proportional-derivative)")
+            Kd = num(1)
+            kp = num(2)
+            ki = 0
+    
+        % No match for PID
+        else
+            error("No PID detected")
+        end
 
-    % Transfer for PI:
-    % PI(s) = ( s*Kp + KI ) / s
-    elseif ((length(num) == 2) && (den(1) == 1) && (den(end) == 0))
-        disp("PI (proportional-integral)")
-        Kp = num(1)
-        Ki = num(2)
-        kd = 0
-    % Transfer for PD:
-    % PD(s) = s*Kd + Kp
-    elseif ((length(num) == 2) && isempty(den))
-        disp("PD (proportional-derivative)")
-        Kd = num(1)
-        kp = num(2)
-        ki = 0
-
-    % No match for PID
+    % Digital PID
     else
-        error("No PID detected")
+        %PID(Z) = ( Z^2*(Kp + Ki + Kd) - z*(Kp + 2Kd) + Kd) / (Z*(Z - 1))
+        if (length(num) == 3)
+            disp("PID Digital (Proportional-integral-derivative)")
+            Kd = num(3)
+            Kp = - num(2) - 2*num(3)
+            Ki = num(1) + num(2) + num(3)
+        
+        %PI(Z)
+        elseif (length(num) == 2)
+            disp("PI Digital (proportional-integral)")
+            Kp = -num(2)
+            Ki = num(1) + num(2)
+            Kd = 0
+        else
+            error("No PID detected")
+        end
     end
 end
